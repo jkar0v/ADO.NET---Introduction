359394df43139638bb6501487e06495504090775
zala2 <zala2@MGKZ2RM07>
2026-02-11 17:32:04 +0200

    Add project files.

---
+++ zad 1/DatabaseHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_1
+{
+    internal class DatabaseHelper
+    {
+        private string connectionString =
+            "Server=localhost\\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public DataTable getStudents()
+        {
+            DataTable table = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT Name, Age, Grade, Mark FROM Students";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                table.Load(dr);
+            }
+            return table;
+        }
+    }
+}
